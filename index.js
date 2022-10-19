12be3a480455a5b6e09e8a5c3192bf4d2ca75a9b
Midokhalil1 <midokhalil22@gmail.com>
2022-10-19 16:08:25 -0400

    NEW

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import express from "express";
-
+import pg from "pg";
+import creds from "./creds.js";
+const { Pool } = pg;
 const app = express();
 
-app.get("/customers", (req, res) => {
-  res.send("This will be a list of customers...soon...");
+app.get("/customers", async (req, res) => {
+  const pool = new Pool(creds);
+  const customers = await pool
+    .query("SELECT * FROM customers")
+    .catch((err) => res.status(500).send(err));
+  //   pool.query("SELECT * FROM customers");
+  //   res.send("This will be a list of customers...soon...");
+  res.send(customers.rows);
+  pool.end();
 });
 
 app.listen(3030, () => console.log("Listening on http://localhost:3030...."));
